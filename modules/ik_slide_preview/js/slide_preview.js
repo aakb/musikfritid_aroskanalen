e9c3959f017fdf55b0ccafb1379a4dd55e0b4cbb
Troels Ugilt Jensen <tujensen@gmail.com>
2014-03-11 11:05:29 +0100

    Changed live slide to include notice text and fixed problem with the button only working the first time

--- modules/ik_slide_preview/js/slide_preview.js
+++ modules/ik_slide_preview/js/slide_preview.js
@@ -1,21 +1,24 @@
 (function($){
   $(document).ready(function() {
     $('.preview-live-slide').click(function(e) {
+      $("#slide-preview").remove();
+
       e.preventDefault();
+
       var src = $(this).attr('href');
-      
+
       // Build iframe.
       var frame = document.createElement('iframe');
       frame.width = '99%';
       frame.height = '99%';
 
       // Build jQuery UI dialog.
-      $('<div id=\"slide-preview\" style=\"overflow:hidden\"></div>').dialog({
+      $('<div id=\"slide-preview\" style=\"overflow:hidden\" title=\"' + Drupal.t("Notice! This live slide IS NOT a precise representation of the real slide. It can be used to examine transitions between images in the slide.") + '\"></div>').dialog({
         modal: true,
         width: $(window).width() - 100,
         height: $(window).height() - 100,
         resizable: false,
-        draggable: false,
+        draggable: false
       });
 
       // Append iframe to the dialog.
@@ -23,7 +26,7 @@
 
       // Add content to the iframe.
       frame.src = src;
-     
+
       // Add class to body.
       setTimeout(
         function(){
